626a723802bd1bcd332ca15e2a4d4a9dbb18c88f
front-ed <edwinschutjes@gmail.com>
2023-04-23 13:26:58 +0200

    add: main container setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
 import "./App.css";
+import { Container, Flex, VStack, Text } from "@chakra-ui/react";
 
 function App() {
   return (
-    <>
-      <h2>hello</h2>
-    </>
+    <Container maxW="container.xl" padding={0} className="inspect">
+      <Flex h="100vh" py={20}>
+        <VStack
+          w="full"
+          h="full"
+          alignItems="flex-start"
+          p={10}
+          spacing={10}
+          background={"red.300"}
+        >
+          <Text>hoi</Text>
+          <Text>hoi</Text>
+        </VStack>
+        <VStack
+          w="full"
+          h="full"
+          alignItems="flex-start"
+          p={10}
+          spacing={10}
+          background="gray.100"
+        >
+          <Text>hoi</Text>
+          <Text>hoi</Text>
+        </VStack>
+      </Flex>
+    </Container>
   );
 }
 
