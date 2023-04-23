fa5a94286dba1af603b03f2518271906359e3a33
front-ed <edwinschutjes@gmail.com>
2023-04-23 15:48:28 +0200

    add: withDefault extensions

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,46 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import { extendTheme } from "@chakra-ui/react";
+import {
+  extendTheme,
+  withDefaultColorScheme,
+  withDefaultVariant,
+} from "@chakra-ui/react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
 // import custom theme extension
 // import theme from "./theme/index.jsx";
 
-const theme = extendTheme({
-  colors: {
-    brand: {
-      50: "#f5fee5",
-      100: "#e1fbb2",
-      200: "#cdf781",
-      300: "#b8ee56",
-      400: "#a2e032",
-      500: "#8ac919",
-      600: "#71ab09",
-      700: "#578602",
-      800: "#3c5e00",
-      900: "#203300",
+const theme = extendTheme(
+  {
+    colors: {
+      brand: {
+        50: "#f5fee5",
+        100: "#e1fbb2",
+        200: "#cdf781",
+        300: "#b8ee56",
+        400: "#a2e032",
+        500: "#8ac919",
+        600: "#71ab09",
+        700: "#578602",
+        800: "#3c5e00",
+        900: "#203300",
+      },
+    },
+    fonts: {
+      heading: "Montserrat",
+      body: "Inter",
     },
   },
-  fonts: {
-    heading: "Montserrat",
-    body: "Inter",
-  },
-});
+  withDefaultColorScheme({
+    colorScheme: "brand",
+    components: ["Checkbox"],
+  }),
+  withDefaultVariant({
+    variant: "filled",
+    components: ["Input", "Select"],
+  })
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
